e451b1efbff514986e7337fb285ffee672478fab
Compat Team <compatazstack1@outlook.com>
2019-12-06 08:23:59 +0000

    commit1

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Text;
+
+
+namespace ConsoleApp2
+
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstno = 10, secondno = 15;
+            int sum = firstno + secondno;
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+    }
+}
